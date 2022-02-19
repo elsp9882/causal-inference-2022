3cebf5ae615236c8f788df0f7bb98d6455d98007
Elliot Spears <elliotspears@Elliots-MBP.lan>
2022-02-18 18:37:29 -0600

    Update

---
+++ R/output/figure_Incarcerations_by_racegender.R
@@ -0,0 +1,14 @@
+read_csv(here("data/NLSY97_new.csv")) %>%
+  group_by(race, gender) %>%
+  summarize(total_incarcerations = mean(total_incarcerations)) %>%
+  ggplot(aes(race, total_incarcerations, fill = gender)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(
+    x = "Race", 
+    y = "Mean Incarcerations", 
+    fill = "Gender",
+    title = "Mean Number of Incarcerations in 2002 by Race and Gender") +
+  theme_minimal() +
+  scale_fill_economist()
+
+ggsave(here("figures/incarcerations_by_racegender.png"), width=8, height=4.5)
